8a6dddf7ad07d15cec023d028d2ff79c96e42921
niu0217 <1033856427@qq.com>
2023-06-13 13:49:03 +0800

    删除不需要的文件

--- Dev/PythonProgramming_FromEntryToPractice/Chapter10/alice.py
+++
@@ -1,7 +0,0 @@
-filename = 'alice.txt'
-try:
-    with open(filename) as f_obj:
-        contents = f_obj.read()
-except FileNotFoundError:
-    msg = 'Sorry, the file ' + filename + ' does not exist.'
-    print(msg)
